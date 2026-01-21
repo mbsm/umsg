cedf9af44228641da35493298daf6ee368f8a5a2
Matias Bustos <mbustos@testbench.cl>
2026-01-21 00:39:32 -0300

    Chore: Add build/ to .gitignore and remove tracked build artifacts

--- build/tests/CMakeFiles/umsg_tests.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for umsg_tests.
-# This may be replaced when dependencies are built.
--- build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/mbustos/umsg/tests
-# Build directory: /Users/mbustos/umsg/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(AllTests "/Users/mbustos/umsg/build/tests/umsg_tests")
-set_tests_properties(AllTests PROPERTIES  _BACKTRACE_TRIPLES "/Users/mbustos/umsg/tests/CMakeLists.txt;25;add_test;/Users/mbustos/umsg/tests/CMakeLists.txt;0;")
